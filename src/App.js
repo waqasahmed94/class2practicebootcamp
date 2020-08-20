83a13044650fda1831e49774f0b9ed6cddadd552
waqasahmed94 <66775969+waqasahmed94@users.noreply.github.com>
2020-08-20 11:00:21 +0000

    added h2

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
         <h1>Hello World From Muhammad Waqas awan</h1>
+        <h2>and this is the end of class2 bootcamp2020</h2>
     </div>
   );
 }
